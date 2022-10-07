deaeb3daf8f09c94bf012456a7a181821f959de2
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-10-07 18:16:12 -0400

    wasd

---
+++ GAM.js
@@ -0,0 +1,52 @@
+//variables
+var home = document.getElementById("home")
+
+//
+var hme = document.getElementById("hme")
+
+//end of variables
+
+//event listeners
+home.addEventListener("mouseover", h1)
+
+//
+home.addEventListener("mouseleave", n1)
+
+//
+
+//end of event listeners
+//on leave
+window.onblur = function(){
+p1()
+
+
+}
+
+
+
+//end
+
+
+
+
+//highlight
+function h1(){
+	home.style = " color:#70ff96; border:4px solid #70ff96;"
+}
+
+//normal
+function n1(){
+	home.style = "color:#ffffff; border:1px solid transparent;"
+}
+
+//
+function p1(){
+	title.innerHTML = "Home"
+	navi.style = "border:3px solid #70ff96"
+	hme.style = "display: block;"
+	gall.style = "display: none;"
+	cntc.style = "display: none;"
+	ext.style = "display: none;"
+}
+
+//end of style script
