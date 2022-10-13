8e2daf8c58938cf137fa26ab376a2ee818b3e8cd
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-10-13 10:16:53 -0400

    wasd

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-var title=document.getElementById("title"),}function p4(){title.innerHTML="Extraz",navi.style="border:3px solid #ffc370",hme.style="display: none;",gall.style="display: none;",cntc.style="display: none;",ext.style="display: block;"}home.addEventListener("mouseover",h1),gal.addEventListener("mouseover",h2),cnt.addEventListener("mouseover",h3),ex.addEventListener("mouseover",h4),home.addEventListener("mouseleave",n1),gal.addEventListener("mouseleave",n2),cnt.addEventListener("mouseleave",n3),ex.addEventListener("mouseleave",n4),home.addEventListener("click",p1),gal.addEventListener("click",p2),cnt.addEventListener("click",p3),ex.addEventListener("click",p4),window.onblur=function(){p1()};
+var title=document.getElementById("title")
 var home=document.getElementById("home")
 var gal=document.getElementById("gal")
 var cnt=document.getElementById("cnt")
@@ -8,6 +8,24 @@ var hme=document.getElementById("hme")
 var gall=document.getElementById("gall")
 var cntc=document.getElementById("cntc")
 var ext=document.getElementById("ext")
+
+home.addEventListener("mouseover",h1)
+gal.addEventListener("mouseover",h2)
+cnt.addEventListener("mouseover",h3)
+ex.addEventListener("mouseover",h4)
+
+home.addEventListener("mouseleave",n1)
+gal.addEventListener("mouseleave",n2)
+cnt.addEventListener("mouseleave",n3)
+ex.addEventListener("mouseleave",n4)
+
+home.addEventListener("click",p1)
+gal.addEventListener("click",p2)
+cnt.addEventListener("click",p3)
+ex.addEventListener("click",p4)
+
+window.onblur=function(){p1()
+
 
 function h1(){home.style=" color:#70ff96; border:4px solid #70ff96;"}
 function h2(){gal.style="color:#70deff; border:4px solid #70deff;"}
@@ -38,4 +56,11 @@ hme.style="display: none;"
 gall.style="display: none;"
 cntc.style="display: block;"
 ext.style="display: none;"
+}
+function p4(){title.innerHTML="Extraz"
+navi.style="border:3px solid #ffc370"
+hme.style="display: none;"
+gall.style="display: none;"
+cntc.style="display: none;"
+ext.style="display: block;"
 }
