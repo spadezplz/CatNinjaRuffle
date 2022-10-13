2f603db317f1da30d44d202ee94ec0289e10d66f
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-10-13 09:57:44 -0400

    wads

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-var title=document.getElementById("title"),",,}function p2(){title.innerHTML="Gallery",navi.style="border:3px solid #70deff",hme.style="display: none;",gall.style="display: block;",cntc.style="display: none;",ext.style="display: none;"}function p3(){title.innerHTML="Contact Us",navi.style="border:3px solid #cf70ff",hme.style="display: none;",gall.style="display: none;",cntc.style="display: block;",ext.style="display: none;"}function p4(){title.innerHTML="Extraz",navi.style="border:3px solid #ffc370",hme.style="display: none;",gall.style="display: none;",cntc.style="display: none;",ext.style="display: block;"}home.addEventListener("mouseover",h1),gal.addEventListener("mouseover",h2),cnt.addEventListener("mouseover",h3),ex.addEventListener("mouseover",h4),home.addEventListener("mouseleave",n1),gal.addEventListener("mouseleave",n2),cnt.addEventListener("mouseleave",n3),ex.addEventListener("mouseleave",n4),home.addEventListener("click",p1),gal.addEventListener("click",p2),cnt.addEventListener("click",p3),ex.addEventListener("click",p4),window.onblur=function(){p1()};
+var title=document.getElementById("title"),}function p4(){title.innerHTML="Extraz",navi.style="border:3px solid #ffc370",hme.style="display: none;",gall.style="display: none;",cntc.style="display: none;",ext.style="display: block;"}home.addEventListener("mouseover",h1),gal.addEventListener("mouseover",h2),cnt.addEventListener("mouseover",h3),ex.addEventListener("mouseover",h4),home.addEventListener("mouseleave",n1),gal.addEventListener("mouseleave",n2),cnt.addEventListener("mouseleave",n3),ex.addEventListener("mouseleave",n4),home.addEventListener("click",p1),gal.addEventListener("click",p2),cnt.addEventListener("click",p3),ex.addEventListener("click",p4),window.onblur=function(){p1()};
 var home=document.getElementById("home")
 var gal=document.getElementById("gal")
 var cnt=document.getElementById("cnt")
@@ -23,4 +23,19 @@ function p1(){title.innerHTML="Home"
  gall.style="display: none;"
  cntc.style="display: none;"
  ext.style="display: none;"
+}
+function p2(){title.innerHTML="Gallery"
+navi.style="border:3px solid #70deff"
+hme.style="display: none;"
+gall.style="display: block;"
+cntc.style="display: none;"
+ext.style="display: none;"
+}
+function p3(){
+title.innerHTML="Contact Us"
+navi.style="border:3px solid #cf70ff"
+hme.style="display: none;"
+gall.style="display: none;"
+cntc.style="display: block;"
+ext.style="display: none;"
 }
