8da8d8f288cb17d9b09f70ec9d65ca3c545796f7
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-10-29 22:35:10 -0400

    w

--- upload.php
+++ upload.php
@@ -1,77 +1,27 @@
 <!DOCTYPE html>
-<html id="ht" lang="en" style="cursor:url('IMG/gcur.png'), auto;">
+<html>
 <head>
-   
-<link rel="icon" type="image/png" href="favicon-32x32.png">
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title id="title">Upload</title>
+  <title>Upload your files</title>
 </head>
 <body>
-   
-
-
-<link rel="stylesheet" href="Home.css" >
-
-  
-<center>
-</center>
-
-<link rel="stylesheet" href="Home.css">
-<nav id="navi" style="border:3px solid #ffc370">
-  
-   <center>
-  <a href="Home.html"><button class="ext" style="min-width: 100%;" id="home">Home</button></a></center>
-</nav>
-
-
-   
-  
-  
-   <div id="dwn" style="display: block;">
-        <center>
-         &nbsp;
-      <br>
-      <br>
-      
-         <p style="border:3px solid #e83a3a">
-            
-            
-     Game links will be here soon...
-
-    <form name="form" method="post" action="upload.php" enctype="multipart/form-data" >
-      <input type="file" name="my_file" /><br /><br />
-      <input type="submit" name="submit" value="Upload"/>
-      </form>
-         </p>
-         
-      </center>
-       </div>
-    
-      
- 
-
-   
-     
-   
-   <script src="Home.js"></script>
+  <form enctype="multipart/form-data" action="upload.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
 </body>
 </html>
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
+<?PHP
+  if(!empty($_FILES['uploaded_file']))
+  {
+    $path = "uploads/";
+    $path = $path . basename( $_FILES['uploaded_file']['name']);
+
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
   }
-}
 ?>
