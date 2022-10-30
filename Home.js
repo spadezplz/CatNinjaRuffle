e68e50b8aedb535d5f46db509524a727cc623af7
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-10-29 21:50:59 -0400

    HoverUpdate

--- Home.js
+++ Home.js
@@ -5,6 +5,7 @@ var cnt = document.getElementById("cnt")
 var ex = document.getElementById("ex")
 var dw = document.getElementById("dw")
 var nav = document.getElementById("navi")
+var tab = 1
 
 var title = document.getElementById("title")
 var ht = document.getElementById("ht")
@@ -80,24 +81,39 @@ function h5(){
 //normal
 function n1(){
 	home.style = "color:#ffffff; border:1px solid transparent;"
+	if(tab === 1){
+		nav.style = "border: 3px solid #70ff96;"
+	}
 }
 function n2(){
 	gal.style = "color:#ffffff; border:1px solid transparent;"
+	if(tab === 2){
+		nav.style = "border:  3px solid #70deff;"
+	}
 }
 function n3(){
 	cnt.style = "color:#ffffff; border:1px solid transparent;"
+	if(tab === 3){
+		nav.style = "border:  3px solid #cf70ff;"
+	}
 }
 function n4(){
 	ex.style = "color:#ffffff; border:1px solid transparent;"
+	if(tab === 4){
+		nav.style = "border:  3px solid #ffc370;"
+	}
 }
 function n5(){
 	dw.style = "color:#ffffff; border:1px solid transparent;"
+	if(tab === 4){
+		nav.style = "border:  3px solid #e83a3a;"
+	}
 }
 //
 function p1(){
 	ht.style = "cursor:url('IMG/gcur.png'), auto;"
 	title.innerHTML = "Home"
-
+tab = 1
 	hme.style = "display: block;"
 	gall.style = "display: none;"
 	cntc.style = "display: none;"
@@ -107,7 +123,7 @@ function p1(){
 function p2(){
 	ht.style = "cursor:url('IMG/bcur.png'), auto;"
 	title.innerHTML = "Gallery"
-	
+	tab = 2
 	hme.style = "display: none;"
 	gall.style = "display: block;"
 	cntc.style = "display: none;"
@@ -117,6 +133,7 @@ function p2(){
 function p3(){
 	ht.style = "cursor:url('IMG/pcur.png'), auto;"
 	title.innerHTML = "Contact Us"
+	tab = 3
 	hme.style = "display: none;"
 	gall.style = "display: none;"
 	cntc.style = "display: block;"
@@ -126,6 +143,7 @@ function p3(){
 function p4(){
 	ht.style = "cursor:url('IMG/ocur.png'), auto;"
 	title.innerHTML = "Online-Arcade"
+	tab = 4
 	hme.style = "display: none;"
 	gall.style = "display: none;"
 	cntc.style = "display: none;"
@@ -135,7 +153,7 @@ function p4(){
 function p5(){
 	ht.style = "cursor:url('IMG/rcur.png'), auto;"
 	title.innerHTML = "Downloads"
-	
+	tab = 5
 	hme.style = "display: none;"
 	gall.style = "display: none;"
 	cntc.style = "display: none;"
