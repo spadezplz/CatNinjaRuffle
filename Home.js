7551b8ff85711cde34f7b1f3885e44cc0e7c3b65
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-10-29 22:40:05 -0400

    e

--- Home.js
+++ Home.js
@@ -219,6 +219,6 @@ function p5(){
 	cntc.style = "display: none;"
 	ext.style = "display: none;"
 	dwn.style = "display: block;"
-	window.open('phantoms-archive.netlify.app/upload.php')
+	window.open(upload.php)
 }
 //end of style script
