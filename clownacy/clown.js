772471add600dae99d0f4dbaa8708d25d55a9f81
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-11-01 10:11:46 -0400

    wasd

--- clownacy/clown.js
+++ clownacy/clown.js
@@ -1,13 +1,7 @@
 setInterval(function(){
     var iframe = document.getElementById("myFrame");
 
-    var elmnt2 = iframe.contentWindow.document.getElementsByTagName("H1")[0];
-    const boxes = document.querySelectorAll('.content');
-  
-    boxes.forEach(box => {
-      box.remove();
-    });
-    elmnt2.style.display = "none";
+    iframe.removeChild();
 
 
 },1)
