543435ab41402d0b108ca1e7929258c5bb47305b
spadezplz <62893208+spadezplz@users.noreply.github.com>
2022-11-30 13:43:24 -0500

    wasd

--- Home.js
+++ Home.js
@@ -222,3 +222,22 @@ function p5(){
 	
 }
 //end of style script
+
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+	  var dropdowns = document.getElementsByClassName("dropdown-content");
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  }
